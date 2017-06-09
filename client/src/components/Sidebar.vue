4c61bca753b545736e641a14f852d2d35223c6f6
erwinwahyura <you@example.com>
2017-06-09 09:26:39 +0700

    adding content for client side

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,55 @@
+<template lang="html">
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <h2>SIDEBAR</h2>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="list-group">
+               <a href="#" class="list-group-item active">
+                 Cras justo odio
+               </a>
+               <a href="#" to="./sidebar" class="list-group-item" >Dapibus ac facilisis in</a>
+               <a href="#" class="list-group-item">Morbi leo risus</a>
+               <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+               <a href="#" class="list-group-item">Vestibulum at eros</a>
+             </div>
+           </div>
+          </div>
+        </div>
+        <div class="col-md-9">
+          <Content></Content>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Sidebar from '@/components/Sidebar'
+import Content from '@/components/Content'
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    subMenu () {
+      panel:''
+    }
+  },
+  components: {
+    Content,
+    Sidebar
+  }
+}
+</script>
+
+<style lang="css" scoped>
+.col {
+  width: 100% !important;
+}
+</style>
